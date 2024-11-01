19aba62ff97651600dde22421023621ec8e9d3c0
Adam Sabbah <Adam Sabbah>
2024-11-02 01:18:34 +0300

    updates

--- database/migrations/2024_11_01_215005_create_notes_table.php
+++ database/migrations/2024_11_01_215005_create_notes_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable();
-            $table->json('note')->nullable();
+            $table->string('title');
+            $table->text('note');
             $table->timestamps();
             $table->softDeletes();
         });
